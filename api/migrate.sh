f999f5a7d3435b43619e9bd7629948e9d6b8a298
Anton Ödman <anton.odman@gmail.com>
2021-11-14 18:44:29 +0100

    Updated migrate script

--- api/migrate.sh
+++ api/migrate.sh
@@ -6,6 +6,11 @@ if [ "$(uname)" == "Darwin" ]; then
     sed -i '' 's/localhost/db/g' ./prisma/.env
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     sed -i 's/db/localhost/g' ./prisma/.env
+    sed -i 's/db:3306/localhost:3306/g' /etc/environment
+    source /etc/environment
+
     npx prisma migrate dev
     sed -i 's/localhost/db/g' ./prisma/.env
+    sed -i 's/localhost:3306/db:3306/g' /etc/environment
+    source /etc/environment
 fi
