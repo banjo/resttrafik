788c4a37604ba94146c6ef7936e628339bdbf922
Anton Ödman <anton.odman@gmail.com>
2021-11-18 18:43:33 +0100

    Fixed mobile version

--- web/src/components/DelayCard/DelayCard.tsx
+++ web/src/components/DelayCard/DelayCard.tsx
@@ -29,30 +29,30 @@ const getDate = (delay: Delay) => {
 export const DelayCard = (props: DelayCardProps) => {
     const { delay } = props;
     return (
-        <div className="w-2/3 h-40 bg-bluegray-700 rounded-xl border-none text-xl relative">
-            <span className="absolute left-4 top-4 font-bold text-2xl">
+        <div className="w-10/12 md:w-2/3 h-40 bg-bluegray-700 rounded-xl border-none text-xl relative">
+            <span className="absolute left-4 top-4 font-bold text-xl md:text-2xl">
                 {delay.type}
             </span>
-            <span className="absolute top-11 left-4 text-base text-gray-400">
+            <span className="absolute top-11 left-4 text-sm md:text-base text-gray-400">
                 {getDate(delay)}
             </span>
 
-            <span className="text-red-600 absolute top-4 right-4 text-2xl font-bold line-through">
+            <span className="text-red-600 absolute top-4 right-4 text-xl md:text-2xl font-bold line-through">
                 {getTime(delay.expectedTime)}
             </span>
-            <span className="text-green-400 absolute top-4 right-24 text-2xl font-bold">
+            <span className="text-green-400 absolute top-4 right-20 md:right-24 text-xl md:text-2xl font-bold">
                 {getTime(delay.newTime)}
             </span>
 
-            <span className="absolute bottom-4 left-4 text-base text-gray-400">
+            <span className="absolute bottom-4 md:bottom-4 left-4 text-sm md:text-base text-white">
                 {getTravelPath(delay)}
             </span>
 
-            <span className="absolute left-4 bottom-9 text-base font-bold ">
+            <span className="absolute left-4 bottom-9 md:bottom-9 text-sm md:text-base font-bold ">
                 {delay.info}
             </span>
 
-            <span className="absolute right-4 bottom-4 font-bold">
+            <span className="absolute right-4 bottom-4 text-base md:text-2xl font-bold">
                 {"Spår: " + delay.track}
             </span>
         </div>
